b0f4a81dded4b068356076d8a2a38fff746b2544
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-03-25 16:38:18 -0300

    setting parcel logic

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -1,17 +1,14 @@
 class ParcelsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_parcel, only: [:show, :edit, :update, :destroy]
+  before_action :set_parcel, only: [:edit, :update, :destroy]
   before_action :set_office_visit, only: [:index, :create]
 
 
   def index
     @parcels = Parcel.where(office_visit: @office_visit).paginate(page: params[:page], per_page: 5)
 
-    if params[:id].present?
-      @parcel = Parcel.find(params[:id])
-    else
-      @parcel = Parcel.new
-    end
+    @parcel = Parcel.new
+
 
     if params[:page].present? && params[:page] > "1"
       @count = (params[:page].to_i - 1) * 5
@@ -20,31 +17,14 @@ class ParcelsController < ApplicationController
     end
   end
 
-  def edit
-    redirect_to parcels_path(id: @office_visit)
-  end
-
   def create
     @parcel = Parcel.new(parcel_params)
     @parcel.office_visit = @office_visit
 
-      if @parcel.save
-        redirect_to request.referrer, notice: 'Parcel was successfully created.'
-      else
-        redirect_to request.referrer, notice: 'Erro ao salvar parcela.'
-      end
-  end
-
-
-  def update
-    respond_to do |format|
-      if @parcel.update(parcel_params)
-        format.html { redirect_to @parcel, notice: 'Parcel was successfully updated.' }
-        format.json { render :show, status: :ok, location: @parcel }
-      else
-        format.html { render :edit }
-        format.json { render json: @parcel.errors, status: :unprocessable_entity }
-      end
+    if @parcel.save
+      redirect_to request.referrer, notice: 'Parcel was successfully created.'
+    else
+      redirect_to request.referrer, notice: 'Erro ao salvar parcela.'
     end
   end
 
@@ -52,7 +32,7 @@ class ParcelsController < ApplicationController
   def destroy
     @parcel.destroy
     respond_to do |format|
-      format.html { redirect_to parcels_url, notice: 'Parcel was successfully destroyed.' }
+      format.html { redirect_to request.referrer, notice: 'Parcel was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -69,6 +49,6 @@ class ParcelsController < ApplicationController
 
 
     def parcel_params
-      params.fetch(:parcel, {})
+      params.require(:parcel).permit(:value, :observation)
     end
 end
