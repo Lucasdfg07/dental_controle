1178d374b07adb9a1f941f86132511eeb87e31e4
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-03-29 22:03:25 -0300

    altering graph

--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -2,8 +2,8 @@ module GraphHelper
 	def week_data
 	    (4.weeks.ago.to_date..Date.today).map do |date| {
 	      created_at: date,
-	      collected: parcels_value(date),
-	      spent: spent_value(date)
+	      collected: OfficeVisit.joins(:patient).joins(:parcels).where("date(office_visits.created_at) = ? AND patients.user_id = ?", date, current_user.id).sum("parcels.value"),
+	      spent: Spent.where("date(created_at) = ? AND user_id = ?", date, current_user.id).sum(:value)
 	    }
 	    end
   	end
@@ -11,8 +11,8 @@ module GraphHelper
   	def month_data
 	    (4.months.ago.to_date..Date.today).map do |date| {
 	      created_at: date,
-	      collected: parcels_value(date),
-	      spent: spent_value(date)
+	      collected: OfficeVisit.joins(:patient).joins(:parcels).where("date(office_visits.created_at) = ? AND patients.user_id = ?", date, current_user.id).sum("parcels.value"),
+	      spent: Spent.where("date(created_at) = ? AND user_id = ?", date, current_user.id).sum(:value)
 	    }
 	    end
   	end
@@ -20,17 +20,9 @@ module GraphHelper
   	def annual_data
 	    (4.years.ago.to_date..Date.today).map do |date| {
 	      created_at: date,
-	      collected: parcels_value(date),
-	      spent: spent_value(date)
+	      collected: OfficeVisit.joins(:patient).joins(:parcels).where("date(office_visits.created_at) = ? AND patients.user_id = ?", date, current_user.id).sum("parcels.value"),
+	      spent: Spent.where("date(created_at) = ? AND user_id = ?", date, current_user.id).sum(:value)
 	    }
 	    end
-  	end
-
-  	def parcels_value(date)
-  		OfficeVisit.joins(:patient).joins(:parcels).where("date(office_visits.created_at) = ? AND patients.user_id = ?", date, current_user.id).count
-  	end
-
-  	def spent_value(date)
-  		Spent.where("date(created_at) = ? AND user_id = ?", date, current_user.id).sum(:value)
   	end
 end
